4225046798016e3a3758f260c2fbebef89332b60
LIONORLIX <71428910+LIONORLIX@users.noreply.github.com>
2024-02-22 02:37:31 +0000

    update basic framework

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Model from './model/model.js'
+import Chart from './chart/chart.js'
+
+function App() {
+  return (
+  <div>
+    <Chart/>
+    <Model/>
+    </div>
+  );
+}
+
+export default App;
