91ba60488ebb5c9729bfa1102375d616dcf6cad8
LIONORLIX <x.li0220231@arts.ac.uk>
2024-02-23 15:01:51 +0000

    learn mnist

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-// https://codelabs.developers.google.com/codelabs/tfjs-training-classfication?hl=en#4
-import * as tf from '@tensorflow/tfjs';
+import {MnistData} from './data.js';
 
-export function getModel() {
+async function showExamples(data) {
+  // Create a container in the visor
+  const surface =
+    tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});  
+
+  // Get the examples
+  const examples = data.nextTestBatch(100);
+  const numExamples = examples.xs.shape[0];
+  
+  // Create a canvas element to render each example
+  for (let i = 0; i < numExamples; i++) {
+    const imageTensor = tf.tidy(() => {
+      // Reshape the image to 28x28 px
+      return examples.xs
+        .slice([i, 0], [1, examples.xs.shape[1]])
+        .reshape([28, 28, 1]);
+    });
+    
+    const canvas = document.createElement('canvas');
+    canvas.width = 28;
+    canvas.height = 28;
+    canvas.style = 'margin: 4px;';
+    await tf.browser.toPixels(imageTensor, canvas);
+    surface.drawArea.appendChild(canvas);
+
+    imageTensor.dispose();
+  }
+}
+
+async function run() {  
+    const data = new MnistData();
+    await data.load();
+    await showExamples(data);
+    const model = getModel();
+    tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+  
+    await train(model, data);
+
+    await showAccuracy(model, data);
+    await showConfusion(model, data);
+}
+
+document.addEventListener('DOMContentLoaded', run);
+
+
+function getModel() {
     const model = tf.sequential();
     
     const IMAGE_WIDTH = 28;
@@ -62,7 +106,7 @@ export function getModel() {
     return model;
   }
 
-  export async function train(model, data) {
+  async function train(model, data) {
     const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
     const container = {
       name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
@@ -96,4 +140,37 @@ export function getModel() {
       shuffle: true,
       callbacks: fitCallbacks
     });
+  }
+
+
+  const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+  function doPrediction(model, data, testDataSize = 500) {
+    const IMAGE_WIDTH = 28;
+    const IMAGE_HEIGHT = 28;
+    const testData = data.nextTestBatch(testDataSize);
+    const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+    const labels = testData.labels.argMax(-1);
+    const preds = model.predict(testxs).argMax(-1);
+  
+    testxs.dispose();
+    return [preds, labels];
+  }
+
+  async function showAccuracy() {
+    const [preds, labels] = doPrediction();
+    const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+    const container = { name: 'Accuracy', tab: 'Evaluation' };
+    tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+  
+    labels.dispose();
+  }
+
+  async function showConfusion() {
+    const [preds, labels] = doPrediction();
+    const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+    const container = { name: 'Confusion Matrix', tab: 'Evaluation' };
+    tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+  
+    labels.dispose();
   }
