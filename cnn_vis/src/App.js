3bdbee825b33b3f51f754a1c0622610cb980d1ff
LIONORLIX <x.li0220231@arts.ac.uk>
2024-02-24 01:59:03 +0000

    add monitor widgets

--- cnn_vis/src/App.js
+++ cnn_vis/src/App.js
@@ -3,11 +3,15 @@
 
 import logo from './logo.svg';
 import React, { useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis'
 import './App.css';
 import './chart.css';
 
 import Chart from './chart/chart.js'
-import data from "./data.json";
+import Monitor from "./methods/monitor.js"
+import {MnistData} from './methods/data.js';
+import { showExamples, getModel, train, showAccuracy, showConfusion, run } from './methods/script.js';
 
 function App() {
   const [items, setItems] = useState([]);
@@ -15,15 +19,29 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    setItems(data);
-    // fetch("http://localhost:8000/message")
-    //   .then((res) => res.json())
-    //   .then((data) => setMessage(data.message));
+    
+    (async ()=>{
+      console.log('2');
+      const data = new MnistData();
+      console.log('hi');
+      await data.load();
+      await showExamples(data);
+      const model = getModel();
+      tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+  
+      await train(model, data);
+
+      await showAccuracy(model, data);
+      await showConfusion(model, data);
+    
+    })();
+  
   }, []);
 
   return (
     <div>
       <Chart/>
+      {/* <Monitor/> */}
     </div>
   );
 }
---
+++ cnn_vis/src/methods/monitor.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function Monitor(props){
+    return (
+        <div>
+      <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"></script>
+
+  
+  <script src="./methods/data.js" type="module"></script>
+
+  
+  <script src="./methods/script.js" type="module"></script>
+    </div>
+    )
+}
+
+export default Monitor
