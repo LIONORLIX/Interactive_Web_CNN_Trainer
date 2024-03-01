9a414ecdcde856a42854225f305b7ebcb01bcf37
LIONORLIX <x.li0220231@arts.ac.uk>
2024-03-01 22:56:45 +0000

    add curve

--- cnn_vis/src/chart/chart.js
+++ cnn_vis/src/chart/chart.js
@@ -44,63 +44,70 @@ function Chart(props) {
 
       const nodes = svg.append("g")
 
+      let connection = nodes.selectAll(".connection")
+        .data(neuronData)
+        .enter()
+        .append("g")
+        .attr("class", function (d, i) { return "connection" })
+        .attr("transform", function (d, i) { return "translate(" + (d.layerConv2DIndex * 200 + 220) + "," + (d.neuronIndex * 50 + 20) + ")"; });
+
+      let path = connection.selectAll(".path")
+        .data(function (d, i) {
+          let formerNeurons = [];
+          let dual_array = [];
+          let num = 0;
+          for (let i = 0; i < d.prevNeuronCount; i++) {
+
+            dual_array.push(num);
+            dual_array.push(d.neuronIndex);
+            formerNeurons.push(dual_array);
+            dual_array = [];
+            num += 1;
+
+          }
+          return formerNeurons;
+        })
+        .enter()
+        .append("path")
+        .attr("class", function (d, i) { return "path" })
+        .join("path")
+        .attr("d", function (d) {
+          let x1 = -200;
+          let y1 = 50 * d[0] - 50 * d[1];
+          let x2 = 0;
+          let y2 = 0;
+          return "M " + x1 + " " + y1 + " " + "C " + (x1 + 80) + " " + y1 + "," + (x2 - 80) + " " + y2 + "," + x2 + " " + y2
+        })
+        .attr("stroke-width", 1)
+        .attr("stroke", "gray")
+        .attr("fill", "transparent")
+
       let neuron = nodes.selectAll(".neuron")
         .data(neuronData)
         .enter()
-           .append("g")
-             .attr("class",function (d,i) { return "neuron_"+i })
-             .attr("transform", function(d, i) {return "translate(" + (d.layerConv2DIndex * 200 + 200) + "," +  (d.neuronIndex * 50 + 50) + ")"; });
-      
-      let path = neuron.selectAll(".path")
-             .data(function (d,i) { 
-               let formerNeurons = [];
-               let dual_array = [];
-               let num = 0;
-               for (let i=0;i<d.prevNeuronCount;i++){
-                 
-                 dual_array.push(num);
-                 dual_array.push(d.neuronIndex);
-                 formerNeurons.push(dual_array);
-                 dual_array = [];
-                 num += 1;
+        .append("g")
+        .attr("class", function (d, i) { return "neuron_" + i })
+        .attr("transform", function (d, i) { return "translate(" + (d.layerConv2DIndex * 200 + 220) + "," + (d.neuronIndex * 50 + 20) + ")"; })
 
-               }
-               return formerNeurons;
-             })
-             .enter()
-             .append("line")
-                 .attr("class",function (d,i) { return "path" })
-                 .join("line")
-                 .attr("x1", 0)
-                 .attr("y1", 0)
-                 .attr("x2", -200)
-                 .attr("y2", function(d){
-                  return 50*d[0]-50*d[1]
-                })
-                 .attr("stroke-width", 1)
-                 .attr("stroke", "gray")
-                 .attr("transform", function(d, i) {return "translate(20,20)"; })
-                 //  .moveTo(10, 10)
-                 //   .bezierCurveTo(95, 10, 50, 90, 10, 10);
 
       let row = neuron.selectAll(".row")
         .data(function (d) { return d.tensor2D })
-        .enter()  
-           .append("g")
-             .attr("class", "row")
-             .attr("transform", function(d, i) { return "translate(0," + i * 2 + ")"; });
-      
+        .enter()
+        .append("g")
+        .attr("class", "row")
+        .attr("transform", function (d, i) { return "translate(0," + i * 1.5 + ")"; });
+
       let pixel = row.selectAll(".pixel")
-      .data(function (d,i) { return d })
-        .enter() 
+        .data(function (d, i) { return d })
+        .enter()
         .append("rect")
         .attr("class", "pixel")
-             .attr("x", function(d, i) { 
-              return i*2; 
-            })
-             .attr("width", 2)
-             .attr("height", 2)
-             .attr("fill", function(d) { return "rgb("+ d*255 + ","+ d*255 + ","+ d*255 + ")"; });
+        .attr("x", function (d, i) {
+          return i * 1.5;
+        })
+        .attr("width", 1.5)
+        .attr("height", 1.5)
+        .attr("fill", function (d) { return "rgb(" + d * 255 + "," + d * 255 + "," + d * 255 + ")"; });
 
     })();
   }, [props.testValue]
